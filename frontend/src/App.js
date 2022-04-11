1e89f6468c765a2e15f302bfa1e44aa759782f0c
dineshGargCreole <dinesh.garg@creolestudios.com>
2022-04-11 18:48:47 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 function App() {
   const cart = useSelector(state => state.cart);
   const {cartItems} = cart;
+  const userSignin = useSelector(state => state.userSignin);
+  const {userInfo} = userSignin;
+  const dispatch = useDispatch()
+
+  const signoutHandler = () => {
+    dispatch(signout())
+  }
 
   return (
     <BrowserRouter>
@@ -23,13 +32,27 @@ function App() {
                       <span className="badge">{cartItems.length}</span>
                     )}
                   </Link>
-                  <Link to="/signin">Sign In</Link>
+                  {
+                    userInfo ? (
+                      <div className='dropdown'>
+                      <Link to='#'>
+                        {userInfo.name} <i className='fa fa-caret-down'></i>{' '}
+                      </Link>
+                      <ul className='dropdown-content'>
+                        <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                      </ul>
+                      </div>
+                    ) : (
+                      <Link to="/signin">Sign In</Link>
+                    )
+                  }
                 </div>
           </header>
           <main>
             <Routes>
               <Route path="/cart/:id/:qty" element={<CartScreen />} />
               <Route path='/product/:id' element={<ProductScreen />} />
+              <Route path='/signin' element={<SigninScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </main>
